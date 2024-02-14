1e866eaefa32fefce3149f80a630e7533a4b4f74
Oluwaseun-ayeni <72kulbranking@gmail.com>
2024-02-14 09:19:52 -0600

    new

---
+++ Alphabet.cpp
@@ -0,0 +1,8 @@
+//
+//  Alphabet.cpp
+//  uorcplus
+//
+//  Created by Oluwaseun Ayeni on 2024-02-12.
+//
+
+#include <stdio.h>
---
+++ pyramid.cpp
@@ -0,0 +1,8 @@
+//
+//  pyramid.cpp
+//  uorcplus
+//
+//  Created by Oluwaseun Ayeni on 2024-01-30.
+//
+
+#include <stdio.h>
